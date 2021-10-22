42c849d984df55fb703c7972393b61b7ef63a8a0
Enes Baltacı <enesniyazi.baltaci@ogr.deu.edu.tr>
2021-10-22 12:40:10 +0300

    React Homework

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ console.log("Merhaba Kodlama.io");
 let dolarBugun = 9.30;
 let dolarDun = 9.20;
 
+let student = { id: 2019510103, name: "Enes" };
+
 {
     let dolarDun = 9.10;
 }
@@ -23,7 +25,60 @@ for(let i = 0; i < konutKredileri.length; i++)
 }
 console.log("</ul>");
 
+function save(point = 10, ogrenci)
+{
+    console.log(ogrenci.name + ": " + point);
+}
+
+let adr = [{"id": 1, "name": "Enes B.", "username": "Vabregas",
+            "E-Mail": "vabregasbaltaci@gmail.com",
+            "Address": {"Street": "Necati Molder", "Suite": "Baltaci Apt.", "City": "Istanbul", "Geo": {"lat": "-37.5453", "lng": "81.324"}}}];
+
+console.log(adr);
+
+save(undefined, student);
+
 console.log(konutKredileri);
 
-console.log("Good Night");
-console.log("Good Hunt");
+// rest
+let showProducts = function (id, ...products)
+{
+    console.log(id);
+    console.log(products);
+}
+
+//console.log(typeof showProducts);
+//showProducts(10, "Elma", "Armut", "Karpuz");
+
+//spread ayristirmak
+let points = [1, 2, 3, 4, 50, 4, 60, 14];
+
+console.log(...points);
+console.log(Math.max(...points));
+console.log(..."ABC", "D", ..."EFG", "H");
+
+//Destructuring
+let populations = [10000, 20000, 30000, [40000, 100000]];
+let [small, medium, high, [veryHigh, max]] = populations;
+console.log(small);
+console.log(medium);
+console.log(high);
+console.log(veryHigh);
+console.log(max);
+
+function someFunction([small1], number) {
+    console.log(small1);
+}
+
+someFunction(populations, 5);
+
+let category = {id:1, name: "İçecek"};
+console.log(category.id);
+console.log(category["name"]);
+
+let {id, name} = category;
+
+console.log(id);
+console.log(name);
+
+//Redux
