4587bf3392ba9f3fd318fbda313ee9e5301cfcab
Enes Baltacı <enesniyazi.baltaci@ogr.deu.edu.tr>
2021-11-01 23:52:36 +0300

    23:52

--- src/functions.js
+++ src/functions.js
@@ -54,3 +54,42 @@ let products = [
 ];
 
 addToCart4(products);
+
+function add(bisey, ...numbers) { // rest operator
+    let total = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        total += numbers[i];        
+    }
+    console.log(total);
+    console.log(bisey);
+}
+
+add(20, 30);
+add(20, 30, 40);
+add(20, 30, 40, 50);
+
+let numbers = [30, 10, 500, 600, 120];
+console.log(Math.max(...numbers));
+
+let [icAnadolu, marmara, karadeniz, [icAnadoluSehirleri]] = [
+    {name: "İç Anadolu", population: "20M"},
+    {name: "Marmara", population: "30M"},
+    {name: "Karadeniz", population: "10M"},
+    [
+        ["Ankara", "Konya"],
+        ["İstanbul", "Bursa"],
+        ["Sinop", "Trabzon"]
+    ]
+];
+
+console.log(icAnadolu.name);
+console.log(icAnadolu.population);
+console.log(icAnadoluSehirleri);
+
+let newProductName, newUnitPrice, newQuantity;
+({productName:newProductName, unitPrice:newUnitPrice, quantity:newQuantity}
+ = {productName:"Elma", unitPrice:10, quantity:20});
+
+console.log(newProductName);
+console.log(newUnitPrice);
+console.log(newQuantity);
